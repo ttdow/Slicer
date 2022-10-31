a890b6ecc02fd5c41d353b1b5d96a7aabc1c786a
ttdow <travis.t.dow@gmail.com>
2022-10-31 10:45:16 -0600

    Added a delay before the game starts to allow the spatial mesh to be created.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.SpatialAwareness;
+using TMPro;
+using static UnityEngine.ParticleSystem;
+using System.Net.Sockets;
+using UnityEditor.Experimental.GraphView;
 
 public class GameManager : MonoBehaviour, IMixedRealitySpeechHandler, IMixedRealityTouchHandler
 {
     #region Fields
+
+    private int gameState;
 
     public AudioSource audioSource;
 
     public Camera player;
 
     public GameObject shell;
+    public TextMeshProUGUI shellText;
+    public GameObject shellInput;
+
     public GameObject suspect;
     public GameObject weapon;
     public GameObject clue;
@@ -101,26 +110,59 @@ public class GameManager : MonoBehaviour, IMixedRealitySpeechHandler, IMixedReal
     // Start is called before the first frame update
     void Start()
     {
+        // Activate speech handling
         CoreServices.InputSystem?.RegisterHandler<IMixedRealitySpeechHandler>(this);
 
+        // Get the BG music audio source
         audioSource = GetComponent<AudioSource>();
 
-        StartGame();
+        // Set the initial game state
+        gameState = 0;
+
+        SetupGame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        SayHello();
+        switch (gameState)
+        {
+            case 0:
+                // SETUP THE SCENE
+                if (CheckReady())
+                {
+                    gameState = 1;
+
+                    StartGame();
+                }
+                break;
+
+            case 1:
+                // BUSINESS AS USUAL
+                break;
+
+            case 2:
+                // YOU WIN
+                break;
+        }
     }
 
     #endregion
 
     #region Methods
 
+    public void SetupGame()
+    {
+        shellText.text = "The game will start shortly. Please move around and look at your surroundings to build a spatial mesh.";
+        shellInput.SetActive(false);
+
+        SpawnRandomCase();
+    }
+
     public void StartGame()
     {
-        SpawnRandomCase();
+        shellInput.SetActive(true);
+        shellText.text = "In the year 3022 SOCIETY has moved beyond the physical world and into the cloud. People live as immortals in the digital world with nothing to fear. You are part of the Slicers, cybersecurity experts who investigate crimes in this world. For the first time in the history of your organization you've been tasked with solving a murder. Not only did someone murder this individual, but every copy of them on the network. Who could have perpetrated this heinous crime? Question the suspects, search your area for clues, and find the killer!";
     }
 
     public void SpawnRandomCase()
@@ -131,12 +173,12 @@ public class GameManager : MonoBehaviour, IMixedRealitySpeechHandler, IMixedReal
         int clueChoice = Random.Range(0, 2);
 
         // Randomly place objects in the scene
-        Vector3 bodyPos = new Vector3(Random.Range(-9.0f, 9.0f), 2.0f, Random.Range(-9.0f, 9.0f));
-        Vector3 whitePos = new Vector3(Random.Range(-9.0f, 9.0f), 2.0f, Random.Range(-9.0f, 9.0f));
-        Vector3 mustardPos = new Vector3(Random.Range(-9.0f, 9.0f), 2.0f, Random.Range(-9.0f, 9.0f));
-        Vector3 peacockPos = new Vector3(Random.Range(-9.0f, 9.0f), 2.0f, Random.Range(-9.0f, 9.0f));
-        Vector3 weaponPos = new Vector3(Random.Range(-9.0f, 9.0f), 2.0f, Random.Range(-9.0f, 9.0f));
-        Vector3 cluePos = new Vector3(Random.Range(-9.0f, 9.0f), 2.0f, Random.Range(-9.0f, 9.0f));
+        Vector3 bodyPos = new Vector3(Random.Range(-9.0f, 9.0f), Random.Range(1.0f, 2.0f), Random.Range(-9.0f, 9.0f));
+        Vector3 whitePos = new Vector3(Random.Range(-9.0f, 9.0f), Random.Range(1.0f, 2.0f), Random.Range(-9.0f, 9.0f));
+        Vector3 mustardPos = new Vector3(Random.Range(-9.0f, 9.0f), Random.Range(1.0f, 2.0f), Random.Range(-9.0f, 9.0f));
+        Vector3 peacockPos = new Vector3(Random.Range(-9.0f, 9.0f), Random.Range(1.0f, 2.0f), Random.Range(-9.0f, 9.0f));
+        Vector3 weaponPos = new Vector3(Random.Range(-9.0f, 9.0f), Random.Range(1.0f, 2.0f), Random.Range(-9.0f, 9.0f));
+        Vector3 cluePos = new Vector3(Random.Range(-9.0f, 9.0f), Random.Range(1.0f, 2.0f), Random.Range(-9.0f, 9.0f));
 
         // Create the avatars
         body = GameObject.Instantiate(bodyPrefab, bodyPos, Quaternion.identity);
@@ -187,6 +229,63 @@ public class GameManager : MonoBehaviour, IMixedRealitySpeechHandler, IMixedReal
         }
     }
 
+    public bool CheckReady()
+    {
+        // Detect when all the objects in the scene stop moving
+        Vector3 totalV = body.GetComponent<Rigidbody>().velocity +
+                     white.GetComponent<Rigidbody>().velocity +
+                     mustard.GetComponent<Rigidbody>().velocity +
+                     peacock.GetComponent<Rigidbody>().velocity +
+                     weapon.GetComponent<Rigidbody>().velocity +
+                     clue.GetComponent<Rigidbody>().velocity;
+
+        if (totalV.magnitude < 1.0f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void HideObjects()
+    {
+        Vector3 totalV = Vector3.zero;
+
+        do
+        {
+            totalV = weapon.GetComponent<Rigidbody>().velocity +
+                     clue.GetComponent<Rigidbody>().velocity;
+
+        } while (totalV.magnitude < 1.0f);
+
+        int dir = Random.Range(0, 8);
+
+        // Hide weapon
+        switch (dir)
+        {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+        }
+    }
+
     public void SayHello()
     {
         if(Vector3.Distance(player.transform.position,  white.transform.position) < 2.0f)
@@ -200,6 +299,16 @@ public class GameManager : MonoBehaviour, IMixedRealitySpeechHandler, IMixedReal
         else if(Vector3.Distance(player.transform.position, peacock.transform.position) < 2.0f)
         {
             peacock.GetComponent<AudioSource>().Play();
+        }
+    }
+
+    public void Accuse()
+    {
+        bool playerGuess = false;
+
+        if (playerGuess)
+        {
+            gameState = 2;
         }
     }
 
